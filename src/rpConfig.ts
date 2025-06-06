92977b547fbd0e9c13baba578863bcad4bd5e1e1
ehsanellahii <ehsanellahiofficial@gmail.com>
2025-06-06 10:58:55 +0500

    fix(rpConfig.ts): update environment variable names for rpID and expectedOrigin

--- src/rpConfig.ts
+++ src/rpConfig.ts
@@ -3,8 +3,8 @@ export function getRpConfig(req: any) {
 
   const isLocal = host.includes('localhost') || host.includes('127.0.0.1');
 
-  const rpID = isLocal ? 'localhost' : process.env.NEXT_PUBLIC_RP_ID || 'password-less-authentication.vercel.app';
-  const expectedOrigin = isLocal ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_RP_ORIGIN || 'https://password-less-authentication.vercel.app';
+  const rpID = isLocal ? 'localhost' : process.env.WEBAUTHN_RP_ID || 'password-less-authentication.vercel.app';
+  const expectedOrigin = isLocal ? 'http://localhost:3000' : process.env.WEBAUTHN_RP_ORIGIN || 'https://password-less-authentication.vercel.app';
 
   return { rpID, expectedOrigin };
 }
